f2c11f5cada438f29b1a11604bf3173e2860695e
Woongbin Choi <woongbinchoi@gmail.com>
2021-04-29 22:22:11 +0900

    app constants remove secret key

--- app_constants.py
+++ app_constants.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 TRIAL_CSV = 'trial.csv'
 MAPS_CSV = 'maps.csv'
 
-JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'e1a16c7320b6b57d7088ba8c6c3d123efc91f82bb8f5e4ee')
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'thisissecretkey')
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
 JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 DATABASE_PATH = os.getenv('DATABASE_URL', 'postgresql://localhost/kart_server')
